84a6d77e0c56c8b03879a48c5670ff18d21231f5
casperr04 <mikrel071204@gmail.com>
2023-09-11 19:50:46 +0200

    feat: Added config.py
    Added config.py along with a Config class to allow for creating and reading config files, in order to easily persist and make it easier to change things such as tokens or the backends URL.

---
+++ stomp_ws/config.py
@@ -0,0 +1,32 @@
+import configparser
+
+# Create a global configuration dictionary
+config = {}
+parser = configparser.ConfigParser(allow_no_value=True)
+
+
+class Config:
+    def __init__(self, token=None, refresh=None, username=None, url=None, keep_token=None):
+        self.token = token
+        self.refresh = refresh
+        self.username = username
+        self.url = url
+        self.keep_token = keep_token if isinstance(keep_token, bool) else False
+
+
+# Function to read and load the configuration file
+def load_config(config_file_path: str) -> dict:
+    global config
+    parser.read(config_file_path)
+    config = dict(parser.items('DEFAULT'))
+    return config
+
+
+def write_config(config_object: Config):
+    parser['DEFAULT'] = {'token': config_object.token,
+                         'refresh': config_object.refresh,
+                         'username': config_object.username,
+                         'backendUrl': config_object.url,
+                         'keep-token': config_object.keep_token}
+    with open('./config.ini', 'w') as configfile:
+        parser.write(configfile)
