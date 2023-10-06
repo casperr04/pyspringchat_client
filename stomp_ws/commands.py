8e259915c33e6b4947e71569efeeb33f9d8cd770
casperr04 <kacperjaroslawurbanczyk@gmail.com>
2023-10-06 16:59:34 +0200

    feat: Added MainCommands for main menu commands

--- stomp_ws/commands.py
+++ stomp_ws/commands.py
@@ -31,3 +31,52 @@ class ChannelCommands:
         self.destination_id = destination_url
         self.disconnected = False
         self.commands = {"/ping": self.ping_command, "/exit": self.exit_command}
+
+
+class MainCommands:
+    def command_handler(self, command: str):
+        commands = self.commands
+        split_commands = str.split(command)
+        if split_commands[0] not in commands:
+            return False
+        if len(split_commands) > 1:
+            return_val = commands[split_commands[0]](split_commands[1])
+        else:
+            return_val = commands[command]()
+        if return_val is None:
+            return None
+        else:
+            return return_val
+
+    def help(self):
+        for k, v in self.commands_help.items():
+            print(f"{k} - {v}")
+        print("\n")
+
+    def join_channel(self, destination_id):
+        from main import chat_print, destination_url_prefix
+        channel_headers, channel_unsubscribe = self.client.subscribe(
+            destination=f"/user/{self.user.username}/queue/chat/{destination_id}",
+            headers={"channel": str(destination_id)},
+            callback=chat_print)
+        channel_commands = ChannelCommands(self.client, channel_headers, channel_unsubscribe,
+                                           f"{destination_url_prefix}{destination_id}",
+                                           destination_id)
+        return channel_headers, channel_unsubscribe, channel_commands
+
+    def __init__(self, client, user):
+        self.commands_help = {"/help": "Get list of commands",
+                              "/join_channel [channel-id]": "Join a specified channel",
+                              "/private-channels": "Get a list of your private channels",
+                              "/user-info {user-id}": "Get info about a user",
+                              "/log-out": "Logs you out.",
+                              "/exit": "Exits PySpringChat"}
+
+        self.commands = {"/help": self.help,
+                         "/join_channel": self.join_channel}
+                         # "/private_channels": self.private_channels,
+                         # "/user_info": self.user_info,
+                         # "/log_out": self.log_out}
+
+        self.client = client
+        self.user = user
