50cbc0f629e581aacd996102246f17bf17b747ac
casperr04 <kacperjaroslawurbanczyk@gmail.com>
2023-10-06 17:01:02 +0200

    feat: Made try_token static

--- stomp_ws/auth.py
+++ stomp_ws/auth.py
@@ -4,19 +4,20 @@ import requests
 from stomp_ws import config
 
 
+def try_token(token):
+    endpoint = f"{config.config.get('backendurl')}/demo/controller"
+    r = requests.get(endpoint, headers={"Authorization": f"Bearer {token}"})
+    if r.status_code != 200:
+        return False
+    else:
+        return r.text
+
+
 class User:
     def __init__(self, username, token=None, user_id=None):
         self.token = token
         self.id = user_id
         self.username = username
-
-    def try_token(self, token):
-        endpoint = f"{config.config.get('backendurl')}/demo/controller"
-        r = requests.get(endpoint, headers={"Authorization": f"Bearer {token}"})
-        if r.status_code != 200:
-            return False
-        else:
-            return True
 
     def login(self, password):
         endpoint = f"{config.config.get('backendurl')}/v1/auth/login"
