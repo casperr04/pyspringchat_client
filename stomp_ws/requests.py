ca56e7a5954f478c94ae99b63b67a690975d2ffc
casperr04 <kacperjaroslawurbanczyk@gmail.com>
2023-10-14 14:46:41 +0200

    refactor : Inject config to Requests class, implement check if user is in channel

--- stomp_ws/requests.py
+++ stomp_ws/requests.py
@@ -1,11 +1,28 @@
 import requests
-from stomp_ws import config
 
 
-def check_server_status():
-    endpoint = f"{config.config.get('backendurl')}/demo/controller/public"
-    r = requests.get(endpoint)
-    if r.status_code != 200:
-        return False
-    else:
-        return True
+class Requests:
+    def __init__(self, config):
+        self.config = config
+        self.backendurl = config.get('backendurl')
+        self.bearer = config.get('token')
+        self.auth = {"Authorization": f"Bearer {self.bearer}"}
+
+    def check_server_status(self):
+        endpoint = f"{self.backendurl}/demo/controller/public"
+        try:
+            r = requests.get(endpoint)
+        except requests.exceptions.ConnectionError:
+            return False
+        if r.status_code != 200:
+            return False
+        else:
+            return True
+
+    def check_if_in_server(self, channel_id):
+        endpoint = f"{self.backendurl}/v1/channels/check/private-channel/{channel_id}"
+        r = requests.get(endpoint, headers=self.auth)
+        if r.status_code != 200:
+            return False
+        else:
+            return True
